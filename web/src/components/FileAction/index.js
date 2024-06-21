8528469bbf369875148674df03308a90eafaf33b
Czz03 <510413980@qq.com>
2024-06-21 08:42:17 +0800

    Delete web/src/components directory

--- web/src/components/FileAction/index.js
+++
@@ -1,4 +0,0 @@
-export { default as FileAction } from './FileAction.vue'
-export { default as DeleteFile } from './components/DeleteFile.vue'
-export { default as FolderSelect } from './components/FolderSelect.vue'
-export { default as ShareFile } from './components/ShareFile.vue'
